1691cbff41f150dec51147725f01c926a9dc61b9
yemiwebby <yemiwebby@gmail.com>
2025-02-25 19:10:33 +0000

    update configuration file

--- lighthouserc.js
+++ lighthouserc.js
@@ -14,3 +14,44 @@ export const ci = {
     target: "temporary-public-storage",
   },
 };
+
+// module.exports = {
+//   ci: {
+//     collect: { url: ["http://localhost:5173"] },
+//     assert: {
+//       assertions: {
+//         "categories:seo": "off",
+//         "categories:best-practices": "off",
+//         "categories:pwa": "off",
+
+//         "categories:performance": ["error", { minScore: 0.6 }],
+//         "categories:accessibility": ["error", { minScore: 0.8 }],
+//       },
+//     },
+//     upload: {
+//       target: "temporary-public-storage",
+//     },
+//   },
+// };
+
+// eslint-disable-next-line no-undef
+// module.exports = {
+//   ci: {
+//     collect: {
+//       url: ["http://localhost:5173"],
+//       staticDistDir: "./dist", // Change to your actual build output folder
+//     },
+//     assert: {
+//       assertions: {
+//         "categories:seo": "off",
+//         "categories:best-practices": "off",
+//         "categories:pwa": "off",
+//         "categories:performance": ["error", { minScore: 0.6 }],
+//         "categories:accessibility": ["error", { minScore: 0.8 }],
+//       },
+//     },
+//     upload: {
+//       target: "temporary-public-storage",
+//     },
+//   },
+// };
